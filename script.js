26700b388006b4d4e0bc48041374dc28d162c315
sohel08 <84220820+sohel08@users.noreply.github.com>
2022-09-20 07:51:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// playAudio when pressing button 
+const playAudio = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+};
+
+// play audio when clicking on boxes
+const playAudioOnClick = (e) => {
+  let target = e.target;
+
+  if (target.classList.length === 0 || target.classList.contains("sound")) {
+    target = target.parentElement;
+  } else target = target;
+  
+  const key = target;
+  const audioKeyCode = target.attributes["data-key"].value;
+  const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`);
+
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+};
+
+// for remove transiotion 
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+}
+
+const allKeys = Array.from(document.querySelectorAll(".key"));
+
+// for remove transiotioned when transiton complete
+allKeys.forEach((key) =>
+  key.addEventListener("transitionend", removeTransition)
+);
+
+// play audio on clicking boxes
+allKeys.forEach((key) => key.addEventListener("click", playAudioOnClick));
+
+// play audio on pressing button
+window.addEventListener("keydown", playAudio);
