47211465480fc38bda6ab1b8c6af35f9dd779481
sohel08 <84220820+sohel08@users.noreply.github.com>
2022-10-08 13:19:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-// playAudio when pressing button 
+// playAudio when pressing button
 const playAudio = (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if (!audio) return;
 
+  // add playing class to the key
   key.classList.add("playing");
   audio.currentTime = 0;
   audio.play();
 };
 
-// play audio when clicking on boxes for mobile or screen touch devices
-const playAudioOnClick = (e) => 
+// play audio when clicking on boxes
+const playAudioOnClick = (e) => {
   let target = e.target;
-
   if (target.classList.length === 0 || target.classList.contains("sound")) {
     target = target.parentElement;
-  } 
-  
+  }
+
   const key = target;
   const audioKeyCode = target.attributes["data-key"].value;
   const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`);
@@ -26,7 +26,7 @@ const playAudioOnClick = (e) =>
   audio.play();
 };
 
-// for remove transiotion 
+// for remove transiotion
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
   e.target.classList.remove("playing");
